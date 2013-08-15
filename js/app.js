fa9150b6605134b42480935d1c26948bf890f96b
Swift <theycallmeswift@gmail.com>
2013-08-15 15:54:22 -0400

    Much closer to launch

--- js/app.js
+++ js/app.js
@@ -1,116 +1,65 @@
-$(document).ready(function() {
-  var source = $("#source");
+(function() {
+  $(document).ready(function() {
+    var source = $("#source")
+      , resize;
 
-  $(document).foundation();
+    var chart = window.chart = new SwiftChart({
+      canvas: "#main-chart"
+    });
 
-  $(window).resize(function() { updateDataTableAndDrawChart(source) });
-  source.on('input', function(e) { updateDataTableAndDrawChart(e.target); });
+    $(document).foundation();
 
-  resizeMainChart();
-  updateDataTableAndDrawChart(source);
+    $(window).resize(function() {
+      chart.resize();
+      chart.redraw({ animation: false, resized: true });
+    });
 
-  $("#download-chart").click(function(e) {
-    var canvas = $("#main-chart").get(0)
-      , downloadUrl = canvas.toDataURL("image/png").replace("image/png", "image/octet-stream");
+    source.on('input', function(e) { updateDataTableAndDrawChart(e.target, chart); });
 
-    e.preventDefault();
+    updateDataTableAndDrawChart(source, chart);
 
-    document.location.href = downloadUrl;
-  });
-});
+    $("#download-chart").submit(function() {
+      var canvas = $("#main-chart").get(0)
+        , downloadUrl = canvas.toDataURL("image/png");
 
-function createTable(headers, rows) {
-  var $dataTable = $(".data-table")
-    , $dataTableTemplate = $("#data-table-template")
-    , template = _.template($dataTableTemplate.html());
+      $("#chart-data").val(downloadUrl);
+      return true;
+    });
 
-  $dataTable.html(template({ headers: headers,rows: rows }));
-}
+    $(".chart-types a").on("click", function(e) {
+      e.preventDefault();
 
-function csvToChartData(labels, csv) {
-  var output = { labels: [], datasets: [] };
+      chart.type = $(this).data("type");
+      chart.redraw();
 
-  for(var h = 0; h < labels.length - 1; h++) {
-    output.datasets.push(getEmptyDataset(h));
-  }
-
-  _.each(csv, function(row) {
-    output.labels.push(row.shift());
-    // TODO: If any row has a different length, this will break
-    for(var i = 0; i < row.length; i++) {
-      var dataset = output.datasets[i]
-      if(dataset) dataset.data.push(parseInt(row[i]));
-    };
+      $(".chart-types a.selected").removeClass("selected");
+      $(this).addClass("selected");
+    });
   });
 
-  return output;
-}
+  function createTable(headers, rows) {
+    var $dataTable = $(".data-table")
+      , $dataTableTemplate = $("#data-table-template")
+      , template = _.template($dataTableTemplate.html());
 
-function drawChart(data) {
-  var $mainChart = $("#main-chart")
-    , ctx = $mainChart.get(0).getContext("2d")
-    , options = {}
-    , chart;
-
-  chart = new Chart(ctx).Bar(data, options);
-}
-
-function getEmptyDataset(index) {
-  var colors = ["0B486B", "3B8183", "ED303C", "FF9C5B", "FAD089",  "40223E", "403B33", "DCDCDC"]
-    , actualIndex = index % colors.length
-    , rgb = hexToRGB(colors[actualIndex]);
-
-  return {
-    fillColor : "rgba(" + rgb[0] + "," + rgb[1] + "," + rgb[2] + ", 0.5)",
-    strokeColor : "rgba(" + rgb[0] + "," + rgb[1] + "," + rgb[2] + ",1)",
-    pointColor : "rgba(" + rgb[0] + "," + rgb[1] + "," + rgb[2] + ", 1)",
-    pointStrokeColor : "#fff",
-    data : []
-  }
-}
-
-function hexToRGB(hex) {
-  if (hex.charAt(0)=="#") {
-    hex = hex.substring(1,7)
+    $dataTable.html(template({ headers: headers,rows: rows }));
   }
 
-  return [
-    parseInt(hex.substring(0, 2), 16),
-    parseInt(hex.substring(2, 4), 16),
-    parseInt(hex.substring(4, 6), 16)
-  ]
-}
+  function updateDataTableAndDrawChart(source, chart) {
+    var $dataTable = $(".data-table")
+      , self = $(source)
+      , chartData, headers;
 
-function resizeMainChart() {
-  var $mainChart = $("#main-chart")
-    , maxWidth, maxHeight;
+    $.csv.toArrays(self.val().trim(), {}, function(err, csvArray){
+      if(err) return console.log(err); // TODO: Update error messages
 
-  maxWidth = $mainChart.parent().width();
-  maxHeight = Math.round(maxWidth * (3/11), 0);
-
-  console.log("Resizing to " + maxWidth + "x" + maxHeight);
-  $mainChart.attr("width", maxWidth);
-  $mainChart.attr("height", maxHeight);
-}
-
-function updateDataTableAndDrawChart(target) {
-  var $dataTable = $(".data-table")
-    , self = $(target)
-    , chartData, headers;
-
-  $.csv.toArrays(self.val().trim(), {}, function(err, csvArray){
-    if(err) {
-      console.log(err);
-      // TODO: Update error messages
-    }
-
-    if(csvArray.length >= 1) {
-      headers = csvArray.shift();
-      createTable(headers, csvArray);
-      drawChart(csvToChartData(headers, csvArray));
-    } else {
-      $dataTable.html("<p>Enter some CSV formated data on the left!</p>");
-    }
-  });
-}
-
+      if(csvArray.length >= 1) {
+        headers = csvArray.shift();
+        createTable(headers, csvArray);
+        chart.draw(csvArray);
+      } else {
+        $dataTable.html("<p>Enter some CSV formated data on the left!</p>");
+      }
+    });
+  }
+})();
---
+++ js/swift_chart.js
@@ -0,0 +1,135 @@
+function SwiftChart(options) {
+  this.canvas = $(options.canvas);
+  this.drawingOptions = {}
+  this.data = {};
+  this.type = options.type || "Bar";
+  this.colors = options.colors || ["97BBCD", "DCDCDC", "3B8183", "ED303C", "FF9C5B", "FAD089",  "40223E", "403B33", "0B486B" ];
+
+  // Initialize Context
+  this.ctx = this.canvas.get(0).getContext("2d");
+
+  // Resize
+  this.resize();
+
+  // Initialize Chart.js Chart
+  this.chart = new Chart(this.ctx);
+}
+
+SwiftChart.prototype.redraw = function(options) {
+  var options = _.defaults(options || {}, this.drawingOptions);
+  this._draw(this.data, options);
+};
+
+SwiftChart.prototype._regenerateChart = function() {
+  var context = this.ctx;
+
+  delete this.chart;
+  this.resize();
+  context.clearRect(0, 0, context.canvas.width, context.canvas.height);
+
+  this.chart = new Chart(this.ctx);
+};
+
+SwiftChart.prototype.draw = function(data, options) {
+  this.data = data || {};
+  this.drawingOptions = options || {};
+
+  this._draw(this.data, this.drawingOptions);
+};
+
+SwiftChart.prototype._draw = function(data, options) {
+  var chartData = this.transform(this.type, data);
+  this._regenerateChart();
+  this.chart[this.type](chartData, options);
+};
+
+SwiftChart.prototype.resize = function() {
+  var maxWidth, maxHeight;
+
+  maxWidth = this.canvas.parent().width();
+  maxHeight = Math.round(maxWidth * (11/38), 0);
+
+  this.canvas.attr("width", maxWidth);
+  this.canvas.css("width", maxWidth + "px");
+  this.canvas.attr("height", maxHeight);
+  this.canvas.css("height", maxHeight + "px");
+};
+
+SwiftChart.prototype.transform = function(type, data) {
+  if(_.contains(["Line", "Bar", "Radar"], type)) {
+    return this._complexTransform(data);
+  } else {
+    return this._simpleTransform(data);
+  }
+};
+
+SwiftChart.prototype._simpleTransform = function(data) {
+  var output = []
+    , actualIndex, label, value;
+
+  for(var i = 0; i < data.length; i++) {
+    actualIndex = i % this.colors.length;
+    if(data[i].length > 1) {
+      label = data[i][0];
+      value = parseInt(data[i][1]);
+    } else {
+      label = undefined;
+      value = parseInt(data[i][0]);
+    }
+
+    output.push({
+      label: label,
+      value: value,
+      color: this.colors[actualIndex]
+    });
+  }
+
+  return output;
+};
+
+SwiftChart.prototype._complexTransform = function(data) {
+  var output = { labels: [], datasets: [] };
+
+  for(var h = 0; h < data[0].length - 1; h++) {
+    // TODO: If any row has a different length, this will break
+    output.datasets.push(this._getEmptyDataset(h));
+  }
+
+  _.each(data, function(row) {
+    output.labels.push(row[0]);
+    for(var i = 1; i < row.length; i++) {
+      var dataset = output.datasets[i - 1]
+      if(dataset) dataset.data.push(parseInt(row[i]));
+    };
+  });
+
+  return output;
+};
+
+SwiftChart.prototype._getEmptyDataset = function(index) {
+  var actualIndex = index % this.colors.length
+    , rgb = hexToRGB(this.colors[actualIndex]);
+
+  return {
+    fillColor : "rgba(" + rgb[0] + "," + rgb[1] + "," + rgb[2] + ", 0.5)",
+    strokeColor : "rgba(" + rgb[0] + "," + rgb[1] + "," + rgb[2] + ",1)",
+    pointColor : "rgba(" + rgb[0] + "," + rgb[1] + "," + rgb[2] + ", 1)",
+    pointStrokeColor : "#fff",
+    data : []
+  }
+};
+
+/* Util Functions */
+
+function hexToRGB(hex) {
+  if (hex.charAt(0)=="#") {
+    hex = hex.substring(1,7)
+  }
+
+  return [
+    parseInt(hex.substring(0, 2), 16),
+    parseInt(hex.substring(2, 4), 16),
+    parseInt(hex.substring(4, 6), 16)
+  ]
+}
+
